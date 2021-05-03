1da0696c8b79032c54dd0bccb775ba35c45d98a0
dicksen <dicksenrayden@gmail.com>
2021-05-03 09:50:33 +0400

    layouts added

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,13 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer class="white" v-model="drawer" app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="display-5 font-weight-bold">
-            Elearning web version
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-      <v-divider></v-divider>
-      <v-list shaped>
-        <v-subheader>MENU</v-subheader>
-        <v-list-item-group v-model="selectedItem" color="primary">
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-            :to="item.route"
-            link
-          >
-            <v-list-item-icon>
-              <v-icon v-text="item.icon"></v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="item.text"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app color="primary" dark>
-      <template v-slot:img="{ props }">
-        <v-img
-          v-bind="props"
-          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
-        ></v-img>
-      </template>
-
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Elearning</v-toolbar-title>
-    </v-app-bar>
-
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-  </v-app>
+  <div id="app">
+    <component :is="this.$route.meta.layout || 'div'">
+      <router-view />
+    </component>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
-
-  data: () => ({
-    drawer: null,
-    selectedItem: 0,
-    items: [
-      { text: 'Home', icon: 'mdi-home', route: '/' },
-      { text: 'Students', icon: 'mdi-account-group', route: '/students' },
-      { text: 'My Courses', icon: 'mdi-account', route: '/my-courses' },
-      { text: 'All Courses', icon: 'mdi-school', route: '/all-courses' },
-    ],
-  }),
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,31 +2,45 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Students from '../views/Students.vue';
+import Login from '../views/Login.vue';
 import MyCourses from '../views/MyCourses.vue';
 import AllCourses from '../views/AllCourses.vue';
+import MainLayout from '../layouts/MainLayout.vue';
+import LoginLayout from '../layouts/LoginLayout.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    meta: { layout: LoginLayout },
+  },
+
+  {
     path: '/',
     name: 'Home',
     component: Home,
+    meta: { layout: MainLayout },
   },
   {
     path: '/students',
     name: 'Students',
     component: Students,
+    meta: { layout: MainLayout },
   },
   {
     path: '/my-courses',
     name: 'MyCourses',
     component: MyCourses,
+    meta: { layout: MainLayout },
   },
   {
     path: '/all-courses',
     name: 'AllCourses',
     component: AllCourses,
+    meta: { layout: MainLayout },
   },
 ];
 
