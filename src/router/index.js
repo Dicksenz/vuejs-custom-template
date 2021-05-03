890754d2eb8d661af485b7749f6d5077928d08d0
dicksen <dicksenrayden@gmail.com>
2021-05-03 11:15:31 +0400

    page not found added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import Students from '../views/Students.vue';
+import PageNotFound from '../views/PageNotFound.vue';
 import Login from '../views/Login.vue';
 import MyCourses from '../views/MyCourses.vue';
 import AllCourses from '../views/AllCourses.vue';
@@ -11,6 +12,13 @@ import LoginLayout from '../layouts/LoginLayout.vue';
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '*',
+    name: 'PageNotFound',
+    component: PageNotFound,
+    meta: { layout: LoginLayout },
+  },
+
   {
     path: '/login',
     name: 'Login',
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-app id="inspire">
+      <v-card
+        height="100%"
+        class="d-flex flex-column justify-center align-center"
+        :color="$vuetify.theme.dark ? 'grey darken-3' : 'grey lighten-4'"
+        flat
+        tile
+      >
+        <div id="title" class="6rem font-weight-bold">404</div>
+        <div id="subtitle" class="grey--text">Page not found</div>
+      </v-card>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default { name: 'PageNotFound' };
+</script>
+
+<style lang="sass">
+#title
+  font-size: 14rem
+#subtitle
+  font-size: 2rem
+
+#error-view h1.text-h1
+  font-size: 14rem !important
+</style>
